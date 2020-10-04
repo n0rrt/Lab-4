2dbf8f66af091900ac2b5cc906639f1cf15839e9
Tim Harris <timthebananalord@gmail.com>
2020-10-04 12:51:49 -0400

    formatting

--- DivSales.h
+++ DivSales.h
@@ -7,14 +7,14 @@ class DivSales
 { 
 private:
 	static double corpSales;
-	double divSales = 0; //divSales must be initialized to zero in order to work correctly, otherwise returns -9.22e+61
+	double divSales = 0; //divSales must be initialized to zero in order to work correctly, otherwise returns garbage data at the memory location that divSales is stored in
+
 
 public:
 	double qtrs[4];
 	void setDivSales(double s)
 	{
 		divSales = divSales + s;
-		
 	}
 	void setCorpSales(double s)
 	{
--- Lab 4.cpp
+++ Lab 4.cpp
@@ -13,26 +13,33 @@ int main() {
 	int quarter = 0;
 
 	ifstream File("SalesFile.txt");
-	while (getline(File, data)) {
+	while (getline(File, data)) 
+	{
 		divisions[division].qtrs[quarter] = stod(data); //set quarter
 		divisions[division].setDivSales(stod(data)); //add quarter to divsales
 		divisions[division].setCorpSales(stod(data)); //add quarter to corpsales
 		iter++; //add iterator
-		if (iter % 4 == 0) {
+		if (iter % 4 == 0) 
+		{
 			division++; //add one to division every 4 loops
 		}
 		quarter++;
 		quarter = quarter % 4; // keep quarter between 0-3
 	}
 	int input = 1;
-	while (input) {
+	cout << setprecision(2);
+	cout << fixed;
+	while (input) 
+	{
 		cout << "Enter a division (1-6) or 0 to quit: ";
 		cin >> input;
-		if (input == 0) {
+		if (input == 0) 
+		{
 			cout << "\tTotal corporate sales: $" << divisions[0].getCorpSales();
 			return 0;
 		}
-		else {
+		else 
+		{
 			cout << "\tDivision #" << input << "\tQuarter 1 sales: $" << divisions[input - 1].qtrs[0] << endl;
 			cout << "\tDivision #" << input << "\tQuarter 2 sales: $" << divisions[input - 1].qtrs[1] << endl;
 			cout << "\tDivision #" << input << "\tQuarter 3 sales: $" << divisions[input - 1].qtrs[2] << endl;
@@ -41,6 +48,4 @@ int main() {
 			cout << "\tDivision #" << input << " total sales are: $" << divisions[input - 1].getDivSales() << endl;
 		}
 	}
-	
-	
 }
